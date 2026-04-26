2cd20aba081ab2e4ce8132db7e12668aebaec7a2
arushi <106735382+arushimanj@users.noreply.github.com>
2026-04-26 19:59:52 -0400

    fixed try catch block in login.js!

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,30 +6,32 @@ export default function Login({ setToken }) {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    const res = await API.post("/auth/login", {
-      email,
-      password
-    });
+    try {
+      const res = await API.post("/auth/login", {
+        email,
+        password
+      });
 
-    localStorage.setItem("token", res.data.token);
-    setToken(res.data.token);
+      localStorage.setItem("token", res.data.token);
+      setToken(res.data.token);
+    } catch (err) {
+      console.log(err.response?.data);
+      alert("Login failed");
+    }
   };
-  try {
-  const res = await API.post("/auth/login", {
-    email,
-    password
-  });
 
-  localStorage.setItem("token", res.data.token);
-  setToken(res.data.token);
-} catch (err) {
-  alert("Login failed");
-}
   return (
     <div className="card">
       <h2>Login</h2>
-      <input placeholder="email" onChange={e => setEmail(e.target.value)} />
-      <input placeholder="password" type="password" onChange={e => setPassword(e.target.value)} />
+      <input
+        placeholder="email"
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="password"
+        type="password"
+        onChange={e => setPassword(e.target.value)}
+      />
       <button onClick={handleLogin}>Login</button>
     </div>
   );
