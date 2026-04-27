fd8dab34e97fa90656fbe138e6039f71f58ebab3
arushi <106735382+arushimanj@users.noreply.github.com>
2026-04-26 20:37:57 -0400

    fixed routes

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./auth.controller");
-
+zzz
 // Routes
 router.post("/register", controller.register);
 router.post("/login", controller.login);
--- backend/src/modules/generator/generator.routes.js
+++ backend/src/modules/generator/generator.routes.js
@@ -1,13 +1,14 @@
-const auth = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
 const controller = require("./generator.controller");
 const authMiddleware = require("../../middleware/authMiddleware");
 
+// apply auth to ALL routes in this file
 router.use(authMiddleware);
 
+// routes
 router.post("/workout", controller.generateWorkout);
+router.post("/week", controller.generateWeek);
 
 module.exports = router;
-router.post("/week", auth, controller.generateWeek);
