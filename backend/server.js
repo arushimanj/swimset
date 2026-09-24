54bb2db7b289665f092e49316774a1b32f0f7229
arushi <106735382+arushimanj@users.noreply.github.com>
2026-09-24 15:44:21 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./src/modules/auth/auth.routes");
 
 const app = express();
 
